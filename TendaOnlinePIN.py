52460d9f316946c3ad37b7d481c566a9d84b7768
Victor <drygdryg2014@yandex.com>
2020-06-06 09:20:31 +0300

    Refactoring: expand the search range, add more deltas

--- TendaOnlinePIN.py
+++ TendaOnlinePIN.py
@@ -1,116 +1,28 @@
 # -*- coding: utf-8 -*-
+import argparse
+from collections import OrderedDict
+import json
+
 try:
-    import requests
+    import py3wifi
 except ImportError:
-    print('''The script needs Requests module\n
-Please install with:\npip3 install requests\nor\n
-pip install requests
+    print('''The script needs py3wifi module\n
+Please install with:\npip3 install py3wifi\nor\n
+pip install py3wifi
 ''')
     exit(1)
-import argparse
 try:
     import WPSpin
 except ImportError:
     print('''The script needs WPSpin.py module\n
 Make sure the WPSpin.py file is in the current directory''')
     exit(1)
-from collections import OrderedDict
-import time
-
-# Distance table
-# BSSID Distance: PIN distances
-distances_table = {
-    8: (195528, 195529, 195784, 195785, 196040, 196041, 199310, 251569, 261064, 261065, 261320, 261321, 261576, 261577, 326051, 326600, 326857, 342251, 603760, 744416, 1181172, 3037371, 3299053, 3836421, 4564992, 4569774, 5281265),
-    16: (391056, 391057, 391312, 391313, 391568, 391569, 456592, 456593, 456848, 456849, 457104, 457105, 48119, 186356, 195528, 195529, 195784, 196041, 244846, 251636, 261064, 261320, 261321, 261576, 522384, 522385, 522640, 856221, 1104633, 1137080, 1209680, 1442237, 1442493, 1496741, 3080583, 5477050, 8685733),
-    24: (586840, 586841, 587096, 587097, 587352, 587353, 596591, 652376, 652377, 652632, 652633, 652888, 652889, 661593, 717912, 717913, 718168, 718169, 718425, 799099, 1198303, 1638277, 1703557, 2162801, 3341903, 5017217, 5672578, 5886069, 7679505),
-    32: (847904, 847905, 848160, 848161, 848416, 848417, 913441, 913696, 913697, 913952, 913953, 929090, 1833549, 1899341, 2209906, 2622086, 2886150, 3548321, 4151875, 4488797, 5868362),
-    40: (43944, 43945, 44201, 58556, 109480, 109481, 109737, 109993, 175017, 175273, 897222, 1043944, 1043945, 1044200, 1044201, 1109480, 1109481, 1109736, 1109737, 1175016, 1175017, 1336381, 1666529, 2094870, 3530979, 4266729, 5413153, 6129682),
-    48: (1239473, 1239729, 1239730, 1239985, 1305009, 1305010, 1305265, 1305521, 1370545, 1370801, 1371057, 1952794, 2593205, 3064204, 3889696, 4667470, 4945902, 6101649, 6325210, 239473, 239729, 239985, 254341, 305009, 305265, 305521, 370545, 370801, 371057, 587097, 587352, 652376, 652377, 652632, 652633, 652889, 1093263, 1117039, 1158799)
-}
-max_bssid_distance = max(distances_table)
 
 
-class AuthError(Exception):
-    pass
-
-
-class InputDataError(Exception):
-    pass
-
-
-class Api:
-    def __init__(self, login, password):
-        self.s = requests.Session()
-        self.__auth(login, password)
-
-    def __auth(self, login, password):
-        a = self.s.post('https://3wifi.stascorp.com/user.php?a=login',
-                        data={'login': login, 'password': password})
-        response = a.json()
-        if response['result']:
-            self.login = login
-            self.password = password
-        else:
-            raise AuthError(response['error'])
-
-    def __reAuth(self):
-        self.__auth(self.login, self.password)
-
-    def __baseRequest(self, action, params):
-        try:
-            result = self.s.post(
-                'https://3wifi.stascorp.com/3wifi.php?a={}'.format(action),
-                data=params,
-                timeout=300
-                ).json()
-        except requests.exceptions.ConnectionError:
-            return self.__baseRequest(action, params)
-        if 'auth' in result and not result['auth']:
-            self.__reAuth()
-            return self.__baseRequest(action, params)
-        if result['result'] is False:
-            if result['error'] == 'cooldown':
-                print('[*] 3WiFi guest account cooldown received, please wait 10 seconds...')
-                time.sleep(10)
-                return self.__baseRequest(action, params)
-            else:
-                raise InputDataError(result['error'])
-        return result
-
-    def find(self, bssid=None, essid=None, key=None,
-             pin=None, sensitivity=False, limit=1):
-        if bssid is None:
-            bssid = ''
-        data = {'essid': essid, 'key': key, 'wps': pin}
-        new_data = {'bssid': bssid}
-        for i, j in data.items():
-            if j is None:
-                new_data[i] = '◯'
-            else:
-                new_data[i] = j
-
-        if sensitivity:
-            new_data['sens'] = 'on'
-
-        result = self.__baseRequest('find', new_data)
-        data = result['data']
-        pages_count = result['page']['count']
-        page = 1
-        if not limit:
-            while page < pages_count:
-                page += 1
-                new_data['page'] = page
-                result = self.__baseRequest('find', new_data)
-                data += result['data']
-        else:
-            if limit > pages_count:
-                limit = pages_count
-            while page < limit:
-                page += 1
-                new_data['page'] = page
-                result = self.__baseRequest('find', new_data)
-                data += result['data']
-        return data
+# Tenda's DeltaMAC -> DeltaPINs hashmap
+# DeltaPINs are sorted by frequency
+deltas_table = {int(k): v for k, v in json.load(open('tenda_deltas.json', encoding='utf-8')).items()}
+max_bssid_distance = max(deltas_table)
 
 
 def mac2dec(mac):
@@ -126,10 +38,9 @@ def dec2mac(mac):
     return mac
 
 
-def decIncMAC(mac, value):
-    '''Decrements or increments MAC address'''
-    mac = mac2dec(mac)
-    mac += value
+def incMAC(mac, value):
+    '''Increments MAC address'''
+    mac = mac2dec(mac) + value
     return dec2mac(mac)
 
 
@@ -142,7 +53,7 @@ def containsAlgo(t_mac, wps_pin, pinGen):
                     '63410372', '63491838', '71294988', '74250226')
     if (wps_pin in common_static) or (wps_pin in tenda_static):
         return 'Static PIN: {}'.format(wps_pin)
-    mac_list = [t_mac, decIncMAC(t_mac, -1), decIncMAC(t_mac, +1)]
+    mac_list = [t_mac, incMAC(t_mac, -1), incMAC(t_mac, +1)]
     for mac in mac_list:
         wpsPins = pinGen.getAll(mac=mac, get_static=False)
         for item in wpsPins:
@@ -151,19 +62,17 @@ def containsAlgo(t_mac, wps_pin, pinGen):
     return False
 
 
-def subBSSID(bssid1, bssid2):
-    '''Subsbtract the last byte of BSSID1 from BSSID2'''
-    bssid1 = [int(i, 16) for i in bssid1.split(':')]
-    bssid2 = [int(i, 16) for i in bssid2.split(':')]
-    res = bssid2[-1] - bssid1[-1]
-    return res
+def subMAC(mac1, mac2):
+    """Substracts the first MAC the second MAC"""
+    mac1, mac2 = mac2dec(mac1), mac2dec(mac2)
+    return mac1 - mac2
 
 
 def createParser():
     parser = argparse.ArgumentParser(
         description='''Experimental online PIN code generator
         for some Tenda devices.
-        Uses 3WiFi Wireless Database to get data.'''
+        Uses 3WiFi Wireless Database to get anchor points.'''
         )
     parser.add_argument(
         'bssid',
@@ -179,12 +88,12 @@ def createParser():
         help='ignore if pin to BSSID is found in 3WiFi'
         )
     parser.add_argument(
-        '-d',
-        '--search-depth',
+        '-a',
+        '--anchors',
         type=int,
-        default=1,
-        help='the number of BSSID\'s used for the search PINs \
-        (set 0 for unlimited). Default: 1'
+        default=0,
+        help='maximum number of anchor BSSIDs used for the search PINs \
+        Default: unlimited'
         )
 
     return parser
@@ -198,40 +107,42 @@ if __name__ == '__main__':
         with open('account.txt', 'r', encoding='utf-8') as file:
             login, password = file.read().strip().split(':')
     except FileNotFoundError:
-        login = input('Input a username for 3WiFi\
-                         (just press Enter to use guest account): ')
-        if login:
-            password = input('Input a password for 3WiFi: ')
-        else:
-            login, password = 'antichat', 'antichat'
+        print('You need to log in to 3WiFi')
+        login = input('Username: ')
+        password = input('Password: ')
         try:
-            session = Api(login, password)
-        except AuthError:
-            print('[-] Authorization failed. Check username and password.')
+            client = py3wifi.Client(login=login, password=password)
+            client.auth()
+        except py3wifi.exceptions.AuthError:
+            print('Authorization failed. Please check username and password.')
             exit(1)
         else:
             with open('account.txt', 'w', encoding='utf-8') as file:
                 file.write('{}:{}'.format(login, password))
+            print('The credentials were written to account.txt')
     else:
         try:
-            session = Api(login, password)
-        except AuthError:
-            print('[-] Authorization failed. Check username and password.')
+            client = py3wifi.Client(login=login, password=password)
+            client.auth()
+        except py3wifi.exceptions.AuthError:
+            print('Authorization failed. Please check username and password.')
             exit(1)
-    print('[+] Authentication is successful')
+    print('Authorization is successful')
 
     if namespace.bssid:
-        source_bssid = namespace.bssid
+        target_bssid = namespace.bssid
     else:
-        source_bssid = input('Please specify the BSSID: ')
-    source_bssid = source_bssid.upper()
+        target_bssid = input('Please specify the BSSID: ')
+    target_bssid = target_bssid.upper()
 
-    a = ':'.join(source_bssid.split(':')[:-1]) + '*'
-    res = session.find(bssid=a, pin='□□□□□□□□')
+    # Generating a mask: 11:22:33:44:55:66 -> 11:22:33:44:5*
+    mask = ':'.join(target_bssid.split(':')[:-1])[:-1] + '*'
+    print('[*] Requesting 3WiFi for "{}"…'.format(mask))
+    res = client.request('find', {'bssid': mask, 'wps': '□□□□□□□□'})['data']
     if not res:
-        print('[-] Not found similar BSSID\'s in the 3WiFi')
+        print('[-] Not found similar BSSIDs in the 3WiFi')
         exit(1)
-    print('[+] Found {} similar BSSID\'s'.format(len(res)))
+    print('[+] Found {} records'.format(len(res)))
 
     # Filtering and processing 3WiFi data
     pinGen = WPSpin.WPSpin()
@@ -239,53 +150,53 @@ if __name__ == '__main__':
     for item in res:
         bssid = item['bssid']
         pin = item['wps']
-        if (not namespace.ignore_pin) and (bssid == source_bssid):
+        if (not namespace.ignore_pin) and (bssid == target_bssid):
             print('The PIN for {} was found in 3WiFi: {}'.format(
-                source_bssid, pin)
+                target_bssid, pin)
             )
             exit(0)
         if not containsAlgo(bssid, pin, pinGen) and (bssid not in data):
             data[bssid] = int(pin[:-1])
 
-    # Dictionary of distance as key and BSSID/PIN as value
+    # Dictionary of deltaMAC as key and BSSID/PIN as value
     distances = {}
 
-    # Calculating all distances
+    # Calculating all deltaMACs
     for bssid, pin in data.items():
-        distance = subBSSID(source_bssid, bssid)
+        distance = subMAC(bssid, target_bssid)
         if (distance != 0) and (distance not in distances) \
                 and (abs(distance) <= max_bssid_distance):
             distances[distance] = {'bssid': bssid, 'pin': pin}
 
-    # Sorting distances as absolutely values
+    # Sorting deltaMACs as absolutely values
     if distances:
         distances = OrderedDict(
             sorted(distances.items(), key=lambda x: abs(x[0]))
             )
+        print('[+] {} anchor points defined'.format(len(distances)))
     else:
-        print('[-] Not found nearby BSSID\'s')
+        print('[-] Not found anchor points')
 
     pins = OrderedDict()
-    depth_limit = 0
-    for dist, value in distances.items():
+    anchor_cnt = 0
+    for deltaMac, value in distances.items():
         bssid = value['bssid']
         pin = value['pin']
         temp_pins = []
-        if abs(dist) not in distances_table:
+        if abs(deltaMac) not in deltas_table:
             continue
-        for d in distances_table[abs(dist)]:
-            if dist > 0:
-                rest_pin = pin - d
+        for deltaPin in deltas_table[abs(deltaMac)]:
+            if deltaMac > 0:
+                rest_pin = pin - deltaPin
             else:
-                rest_pin = pin + d
-            if (rest_pin > 9999999) or (rest_pin < 0):
-                continue
+                rest_pin = pin + deltaPin
+            rest_pin %= int(1e7)
             rest_pin = (str(rest_pin) + str(pinGen.checksum(rest_pin))).zfill(8)
             temp_pins.append(rest_pin)
-        pins[bssid] = {'pins': temp_pins, 'distance': dist}
-        if namespace.search_depth != 0:
-            depth_limit += 1
-            if depth_limit == namespace.search_depth:
+        pins[bssid] = {'pins': temp_pins, 'distance': deltaMac}
+        if namespace.anchors != 0:
+            anchor_cnt += 1
+            if anchor_cnt == namespace.anchors:
                 break
 
     for bssid, value in pins.items():
@@ -297,6 +208,6 @@ if __name__ == '__main__':
             counter = 1
             for pin in pin_list:
                 # Pretty printing
-                space = ' ' * (3-len(str(counter)))
+                space = ' ' * (4-len(str(counter)))
                 print('{}){}{}'.format(counter, space, pin))
                 counter += 1
